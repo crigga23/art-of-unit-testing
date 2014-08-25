030d3b0ed8b678fe80ae21c50ed27fcb7e7025a1
crigga23 <craig.mckay@gmail.com>
2014-08-26 00:36:05 +0100

    art of unit testing - getting started

---
+++ LogAn.Tests/LogAnalyzerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LogAn.Tests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidFileName_validFileLowerCased_ReturnsTrue()
+        {
+            // arrange
+            LogAnalyzer analyzer = new LogAnalyzer();
+            // act
+            bool result = analyzer.IsValidLogFileName("whatever.slf");
+            // assert
+            Assert.IsTrue(result);
+        }
+    }
+}
